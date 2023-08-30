33a49cec4f94342aa9915bcc2fb7ab57dc7aba4e
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-30 14:30:39 -0500

    feat(upload): Implement File Uploading Functionality
    Implemented comprehensive file uploading functionality through the uploadthing library, introducing
    defined file routers with authentication handling and secure uploads using uploadthing/next, Next.js
    route handlers for HTTP requests with ourFileRouter, and a FileUpload component enabling diverse
    file type uploads with integrated previews. Additionally, user-friendly UI components including
    UploadButton, UploadDropzone, and Uploader were generated using uploadthing/react, collectively
    enhancing the application's file upload capabilities.

---
+++ components/file-upload.tsx
@@ -0,0 +1,45 @@
+import '@uploadthing/react/styles.css';
+
+import { X } from 'lucide-react';
+import Image from 'next/image';
+
+import { UploadDropzone } from '@/lib/uploadthing';
+
+import { Button } from './ui/button';
+
+interface FileUploadProps {
+  endpoint: 'serverImage' | 'messageFile';
+  value: string;
+  onChange: (url?: string) => void;
+}
+
+export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+  const fileType = value?.split('.').pop();
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <Button
+          type="button"
+          variant="delete"
+          size="icon"
+          onClick={() => onChange('')}
+          className="absolute right-0 top-0"
+        >
+          <X className="h-4 w-4" />
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <UploadDropzone
+        endpoint={endpoint}
+        onClientUploadComplete={(res) => onChange(res?.[0].url)}
+        onUploadError={(error: Error) => console.log(error)}
+      />
+    </div>
+  );
+};
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -23,6 +24,8 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+
+import { FileUpload } from '../file-upload';
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -60,13 +63,13 @@ export const InitialModal = () => {
 
   return (
     <Dialog open>
-      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+      <DialogContent className="overflow-hidden bg-white p-1 text-black">
         <DialogHeader className="px-6 pt-8">
           <DialogTitle className="text-center text-2xl font-bold">
-            Create your server
+            Customize your server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and a image. You can
+            Give your server a personality with a name and an icon. You can
             always change it later.
           </DialogDescription>
         </DialogHeader>
@@ -74,13 +77,28 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
                 name="name"
                 render={({ field }) => (
                   <FormItem>
+                    <FormMessage className="text-xs" />
                     <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
                       Server Name
                     </FormLabel>
@@ -92,7 +110,18 @@ export const InitialModal = () => {
                         {...field}
                       />
                     </FormControl>
-                    <FormMessage />
+                    <div className="text-xs text-zinc-500">
+                      By creating a server, you agree to Discord&apos;s
+                      <Link href="https://discord.com/guidelines">
+                        <Button
+                          variant="link"
+                          size="sm"
+                          className="px-1 text-xs font-bold text-blue-500"
+                        >
+                          Community Guidelines.
+                        </Button>
+                      </Link>
+                    </div>
                   </FormItem>
                 )}
               />
