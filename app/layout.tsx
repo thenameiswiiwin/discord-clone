93239f3b357f44f35cc4cb5ed0db33022c539a5b
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-29 22:28:56 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+
+const font = Open_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Discord Clone',
+  description:
+    'Fullstack and real-time discord clone, all with servers, channels, video calls, audio calls, editing and deleting messages as well as member roles.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
