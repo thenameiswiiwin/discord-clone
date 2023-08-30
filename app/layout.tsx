7dcd99ae86b0b6d3996f792b19a6525a2ce10581
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-29 22:39:33 -0500

    feat((auth), middleware.ts): authentication
    Setup clerk, auth routes for sign-in and sign-up, and application protection middleware.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import '../styles/globals.css'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import '../styles/globals.css';
 
-const font = Open_Sans({ subsets: ['latin'] })
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+
+const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Discord Clone',
   description:
     'Fullstack and real-time discord clone, all with servers, channels, video calls, audio calls, editing and deleting messages as well as member roles.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Next13 Template</h1>
-    </main>
-  )
-}
